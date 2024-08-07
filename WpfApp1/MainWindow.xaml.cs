a89214dd6b0593ff80df03b5e8e8cb73182e2cbf
ladyy <ladyy@DESKTOP-8L1ULN3>
2024-08-07 01:28:36 -0400

    Add project files.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, IMainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtName.Text) && !ListNames.Items.Contains(txtName.Text))
+            {
+                ListNames.Items.Add(txtName.Text);
+                txtName.Clear();
+            }
+        }
+
+        private void ListNames_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void TxtName_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+
+        }
+    }
+
+}
+
+
+
+
+
+
+
